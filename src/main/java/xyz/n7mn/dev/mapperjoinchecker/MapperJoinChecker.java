b243af153f05178223deb87c068c8e11675fe891
kanasaki15 <kassi_0123@kassi-hp.info>
2021-03-13 19:30:53 +0900

    Initial commit

---
+++ src/main/java/xyz/n7mn/dev/mapperjoinchecker/MapperJoinChecker.java
@@ -0,0 +1,17 @@
+package xyz.n7mn.dev.mapperjoinchecker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MapperJoinChecker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
